37d0a2717ade5cc6c84f72079a89c4b0279e9969
skrininshot <38591071+skrininshot@users.noreply.github.com>
2022-12-22 04:13:40 +0500

    Strafing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using System.Collections;
 
 [RequireComponent(typeof(CapsuleCollider))]
 [RequireComponent(typeof(Rigidbody))]
 public class Player : NetworkBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float strafeDistance = 2f;
-    [SerializeField] private float confusedTime = 3f;
-    [SerializeField] private float movementSpeed = 5f;
-    private int confuseAmount = 0;
+    [SerializeField] private float movementSpeed;
+    private float previousFrameMagnitude;
+
+    [Header("Strafe")]
+    [SerializeField] private float strafeDistance;
+    [SerializeField] private float strafingSpeed;
+    private Vector3 strafingPoint;
+    private bool isStrafing;
 
     [Header("Components")]
     [SerializeField] private FollowCamera cam;
@@ -19,15 +25,31 @@ public class Player : NetworkBehaviour
     void Start()
     {
         if (!isLocalPlayer) return;
+
         cam = Instantiate(cam);
         cam.Host = transform;
-        animator.speed = movementSpeed /5f;
+        animator.speed = movementSpeed / 6f;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (!isLocalPlayer) return;
-        Move();
+        
+        if (isStrafing)
+        {
+            Strafe();
+        }
+        else
+        {
+            Move();
+            if (Input.GetMouseButtonDown(0))
+            {
+                isStrafing = true;
+                SetForwardByCamera();
+                strafingPoint = transform.position + transform.forward * strafeDistance;
+            }
+        }
+        previousFrameMagnitude = rb.velocity.magnitude;
     }
 
     private void Move()
@@ -37,14 +59,30 @@ public class Player : NetworkBehaviour
         animator.SetFloat("vertical", verticalSpeed);
         animator.SetFloat("horizontal", horziontalSpeed);
 
-        Vector3 move = (horziontalSpeed * cam.transform.right) + (verticalSpeed * cam.transform.forward);
-        Vector3 velocity = Vector3.ClampMagnitude(move, 1) * movementSpeed;
-        if (velocity != Vector3.zero)
+        if (horziontalSpeed + verticalSpeed != 0)
         {
-            transform.forward = cam.transform.forward;
-            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+            SetForwardByCamera();
         }
+
+        Vector3 move = (horziontalSpeed * transform.right) + (verticalSpeed * transform.forward);
+        Vector3 velocity = Vector3.ClampMagnitude(move, 1) * movementSpeed;
         velocity.y = rb.velocity.y;
         rb.velocity = velocity;
+    }
+
+    private void Strafe()
+    {
+        rb.velocity = Vector3.ClampMagnitude(strafingPoint - transform.position, strafeDistance) * strafingSpeed;
+
+        if (Mathf.Abs(rb.velocity.magnitude - previousFrameMagnitude) < 0.2f)
+        {
+            isStrafing = false;
+        }
+    }
+
+    private void SetForwardByCamera()
+    {
+        transform.forward = Vector3.forward;
+        transform.localEulerAngles = new Vector3(0, cam.transform.localEulerAngles.y, 0);
     }
 }
