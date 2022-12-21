40cd1d4ac2e8ccb8cf1c9e7a755ca2d158f57cb6
skrininshot <38591071+skrininshot@users.noreply.github.com>
2022-12-21 16:37:36 +0500

    Animation fixes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,22 +6,21 @@ using Mirror;
 public class Player : NetworkBehaviour
 {
     [Header("Movement")]
-    [SerializeField] [Range(1,5)] private float movementSpeed = 3f;
     [SerializeField] private float strafeDistance = 2f;
     [SerializeField] private float confusedTime = 3f;
+    [SerializeField] private float movementSpeed = 5f;
     private int confuseAmount = 0;
 
     [Header("Components")]
     [SerializeField] private FollowCamera cam;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Animator animator;
-    private Transform rotatingOrigin;
 
     void Awake()
     {
         cam = Instantiate(cam);
         cam.Host = transform;
-        rotatingOrigin = Instantiate(new GameObject()).transform;
+        animator.speed = movementSpeed /5f;
     }
 
     void Update()
@@ -34,14 +33,15 @@ public class Player : NetworkBehaviour
     {
         float horziontalSpeed = Input.GetAxis("Horizontal");
         float verticalSpeed = Input.GetAxis("Vertical");
-        animator.SetFloat("speed", verticalSpeed != 0 ? verticalSpeed : Mathf.Abs(horziontalSpeed));
+        animator.SetFloat("vertical", verticalSpeed);
+        animator.SetFloat("horizontal", horziontalSpeed);
 
         Vector3 move = (horziontalSpeed * cam.transform.right) + (verticalSpeed * cam.transform.forward);
         Vector3 velocity = Vector3.ClampMagnitude(move, 1) * movementSpeed;
         if (velocity != Vector3.zero)
         {
-            rotatingOrigin.transform.forward = velocity;
-            transform.localEulerAngles = new Vector3(0, rotatingOrigin.transform.localEulerAngles.y);
+            transform.forward = cam.transform.forward;
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
         }
         velocity.y = rb.velocity.y;
         rb.velocity = velocity;
